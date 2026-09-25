a8dde691fd3026ecc2c2386c6792213ef1f7a61f
Kasun Piyumal <ripp.dmc@gmail.com>
2026-09-25 18:44:56 +0800

    corral 0.8.1

--- Formula/corral.rb
+++ Formula/corral.rb
@@ -11,7 +11,7 @@ class Corral < Formula
   homepage "https://github.com/corral-sh/corral"
   url "https://github.com/corral-sh/corral.git",
       using: :git,
-      tag:   "v0.8.0"
+      tag:   "v0.8.1"
   license "Apache-2.0"
   head "https://github.com/corral-sh/corral.git", using: :git, branch: "main"
 
